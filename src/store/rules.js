e00af9913f1143e7f96c3e3f39c6a0fdd83098d7
Peter T Bosse II <peter@ptb.dev>
2020-07-02 15:21:04 -0400

    Fix canUseDom use in src/store/rules.js

--- src/store/rules.js
+++ src/store/rules.js
@@ -1,12 +1,12 @@
-import {
-  canUseDom,
-  getStyleElement
-} from "../api/index.js"
+import { getStyleElement } from "../api/index.js"
 
 export const rules = (function (RULES) {
   return RULES
-}) (canUseDom
-    ? Array.prototype.slice
+}) (Boolean (
+  typeof window !== "undefined" &&
+    window.document &&
+    window.document.createElement
+) ? Array.prototype.slice
       .call (getStyleElement ().sheet.cssRules)
       .map (function ({ cssText }) {
         return cssText.replace (/[\n ]+/gu, "")
