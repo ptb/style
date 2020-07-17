885ffb802c3ba5f401e1025da3df0e90dada2d9f
Peter T Bosse II <peter@ptb.dev>
2020-07-17 12:11:24 -0400

    Add initial tsconfig.json

--- src/build/get-ancestors.js
+++ src/build/get-ancestors.js
@@ -1,21 +1,36 @@
+/**
+ * @param {string[]} ancestors
+ * @param {string[]} selectors
+ * 
+ * @returns {string[]}
+ */
+
 export function getAncestors (ancestors = [], selectors = []) {
-  return selectors.reduce (function (results, selector) {
-    if (ancestors.length) {
-      const index = selector.indexOf ("&")
+  return selectors.reduce (
+    /**
+     * @param {string[]} results
+     * @param {string} selector 
+     */
 
-      ancestors.forEach (function (ancestor) {
-        results.push (
-          index < 0
-            ? ancestor.concat (" ", selector)
-            : selector
-              .slice (0, index)
-              .concat (ancestor, selector.slice (index + 1))
-        )
-      })
+    function (results, selector) {
+      if (ancestors.length) {
+        const index = selector.indexOf ("&")
 
-      return results
-    }
+        ancestors.forEach (function (ancestor) {
+          results.push (
+            index < 0
+              ? ancestor.concat (" ", selector)
+              : selector
+                .slice (0, index)
+                .concat (ancestor, selector.slice (index + 1))
+          )
+        })
+
+        return results
+      }
 
     return results.concat ([selector])
-  }, [])
+    },
+    []
+  )
 }
--- src/utils/get-index.js
+++ src/utils/get-index.js
@@ -1,3 +1,13 @@
+/**
+ * @param {string[]} array
+ * - An array of strings.
+ * @param {string} input
+ * - The string used to find the index in alphabetical sort order.
+ * 
+ * @returns {number}
+ *   The index of the array to insert the string in alphabetical sort order.
+ */
+
 export function getIndex (array = [], input = "") {
   const count = array.length
 
@@ -8,6 +18,17 @@ export function getIndex (array = [], input = "") {
   if (input.localeCompare (array[count - 1]) >= 0) {
     return count
   }
+
+  /**
+   * 
+   * @param {number} a
+   * - Array index for position one.
+   * @param {number} b 
+   * - Array index for position two.
+   *
+   * @returns {number}
+   *   The index of the midpoint between position one and two.
+   */
 
   function getMidpoint (a, b) {
     return Math.floor ((b - a) / 2) + a
